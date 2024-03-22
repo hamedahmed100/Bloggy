e3c52a349af483548810c1be86a56c14d28df853
Hamed Ahmed <hamedahmedhamed100@gmail.com>
2024-03-22 20:22:17 +0100

    Handle subscription for observal-add-category

--- UI/codepulse/src/app/features/category/add-category/add-category.component.ts
+++ UI/codepulse/src/app/features/category/add-category/add-category.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
   styleUrls: ['./add-category.component.css']
 })
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
 
   constructor(private categoryService: CategoryService) {
@@ -16,13 +17,21 @@ export class AddCategoryComponent {
       urlHandle: ''
     }
   }
+
+  private addCategorySubscription?: Subscription;
+ 
   onFormSubmit(){
-    this.categoryService.addCategory(this.model)
+    this.addCategorySubscription =  this.categoryService.addCategory(this.model)
     .subscribe({
       next:(response)=>{
-        console.log(response);
+        console.log("Category added successfully");
       }
     })
+  }
+
+
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 
 }
