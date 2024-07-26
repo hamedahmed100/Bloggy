699b04936137ee67f30b734704b2486ae41067e5
Hamed Ahmed <hamedahmedhamed100@gmail.com>
2024-07-26 22:40:56 +0200

    Adding Live preview and Image preview

--- UI/codepulse/src/app/app.module.ts
+++ UI/codepulse/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { EditCategoryComponent } from './features/category/edit-category/edit-category.component';
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blog
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
